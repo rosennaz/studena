c021cad6d873452e6d2b92d559d7339397ac413f
rosennaz <rosen.nazarov@gmail.com>
2020-10-23 12:07:06 +0300

    Before the weekend

--- plot_scripts/plot_studena.sh
+++ plot_scripts/plot_studena.sh
@@ -2,6 +2,9 @@
 #File for ploting the water volume of dam Studena till to the current day
 
 
+#mysql studena  -se "SELECT Date,Volume FROM volume  ;">test.csv
+
+
 set title "Water volume of dam Studena"
 
 set xdata time
@@ -20,7 +23,7 @@ set xtics rotate
 set yrange [0:26000000]
 set format y "%5.0f"
 #set format y "%2.0t{/Symbol \327}10^{%L}" -- variant 
-plot '/home/rnz/Pictures/Pernik/Razni/test.csv' using 2:3 with boxes lc rgb  "blue" 
+plot '/home/rnz/Pictures/Pernik/Razni/test.csv' using 1:2 with boxes lc rgb  "blue" 
 
 #gnuplot> set term png             (will produce .png output)
 #gnuplot> set output "printme.png" 
