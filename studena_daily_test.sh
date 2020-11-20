3d9f67af9586ca18bf95db596a3599c98125693f
rosennaz <rosen.nazarov@gmail.com>
2020-11-20 10:48:51 +0200

    change in studena.sh and studena_dayli.sh due to changes text in the site vikpernin

--- studena_daily_test.sh
+++ studena_daily_test.sh
@@ -4,8 +4,8 @@
 
 WDIR="/home/rnz/Pictures/Pernik/scripts"
 INFILE=$WDIR/index.html
-OFILE=$WDIR/obem_for_mysql.csv
-OFILE_CURRENT=$WDIR/obem_for_mysql_current.csv
+OFILE=$WDIR/obem_for_mysql_test.csv
+OFILE_CURRENT=$WDIR/obem_for_mysql_current_test.csv
 DATE=`/bin/date  -I`
 Yesterday=`/bin/date -d "Yesterday" -I`
 
@@ -31,19 +31,19 @@ echo Vol-$VOL,Influx-$INFLUX,Outflux-$OUTFLUX
 
 
 #Insert extracted values into database
-/usr/bin/mysql studena -se "INSERT INTO volume(Date,Volume,Influx,Outflux,Diff_flux) VALUES('$DATE','$VOL','$INFLUX','$OUTFLUX','0');"
+/usr/bin/mysql studena -se "INSERT INTO WATER_VOLUME(Date,Volume,Influx,Ourflux,Diff_flux) VALUES('$DATE','$VOL','$INFLUX','$OUTFLUX','0');"
 
 
 
 #Calculates real influx by differrence from previous day
-VOL1=$(/usr/bin/mysql studena -se "SELECT Volume FROM volume WHERE Date='$Yesterday';")
-VOL2=$(/usr/bin/mysql studena -se "SELECT Volume FROM volume WHERE Date='$DATE';")
+VOL1=$(/usr/bin/mysql studena -se "SELECT Volume FROM WATER_VOLUME WHERE Date='$Yesterday';")
+VOL2=$(/usr/bin/mysql studena -se "SELECT Volume FROM WATER_VOLUME WHERE Date='$DATE';")
 
 DIF=`/usr/bin/bc <<< "($VOL2 -  $VOL1)/86.4"`
 
 echo Vol1-$VOL1,Vol2-$VOL2,DIF-$DIF
 
 #Updates database with real influx by differrence from previous day
-/usr/bin/mysql studena -se "UPDATE  volume SET Diff_flux = '$DIF' WHERE Date='$DATE';"
+/usr/bin/mysql studena -se "UPDATE  WATER_VOLUME SET Diff_flux = '$DIF' WHERE Date='$DATE';"
 
 
